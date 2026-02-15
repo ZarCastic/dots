fb8e8961e1bd2069fad1794d60c19950accab6e1
Tobias Fuchs <tobias.fuchs@wemolo.com>
2026-02-15 17:04:04 +0100

    add tirith

--- .config/zsh/02-exports.zsh
+++ .config/zsh/02-exports.zsh
@@ -1,3 +1,3 @@
-export PATH=$PATH:$HOME/bin:$HOME/.local/bin:$HOME/Scripts:$HOME/go/bin
+export PATH=$PATH:$HOME/bin:$HOME/.local/bin:$HOME/Scripts:$HOME/go/bin:$HOME/.cargo/bin
 export CDPATH=$CDPATH:$HOME:$HOME/Projects
 export EDITOR=nvim
